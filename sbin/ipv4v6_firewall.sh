4b8caf06a22e069e221b9950357eba5130aa63c0
riuzenn <riuzenn@outlook.com>
2026-05-05 17:47:34 +0800

    at工具、严格的防火墙规则、改usblan0的mac、修改zte_mifi里hostapd相关模板

--- sbin/ipv4v6_firewall.sh
+++ sbin/ipv4v6_firewall.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
+iptables -F
+iptables -X
+ip6tables -F
+ip6tables -X
+iptables -t mangle -I FORWARD 1 -o wan1 -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 #ipv4
 # 1. 部署 INPUT 链规则 (设备本体防护)
 iptables -I INPUT 1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -I INPUT 2 -i lo -j ACCEPT
 iptables -I INPUT 3 -i br0 -j ACCEPT
-iptables -I INPUT 4 -j DROP
+iptables -I INPUT 4 -i wan1 -p icmp --icmp-type 3 -j ACCEPT
+iptables -I INPUT 5 -j DROP
 # 2. 部署 FORWARD 链规则 (下挂手机/电脑转发优化)
 iptables -I FORWARD 1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -I FORWARD 2 ! -i wan1 -o wan1 -j ACCEPT
+iptables -I FORWARD 2 -i br0 -o wan1 -j ACCEPT
 iptables -I FORWARD 3 -j DROP
 # 3. 关上大门：将默认策略设为严格丢弃
 iptables -P INPUT DROP
@@ -19,8 +25,7 @@ ip6tables -I INPUT 3 -i br0 -j ACCEPT
 ip6tables -I INPUT 4 -j DROP
 ip6tables -I FORWARD 1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 ip6tables -I FORWARD 2 -p ipv6-icmp -j ACCEPT
-ip6tables -I FORWARD 3 ! -i wan1 -o wan1 -j ACCEPT
-ip6tables -I FORWARD 4 ! -i wan1 ! -o wan1 -j ACCEPT
-ip6tables -I FORWARD 5 -j DROP
+ip6tables -I FORWARD 3 -i br0 -o wan1 -j ACCEPT
+ip6tables -I FORWARD 4 -j DROP
 ip6tables -P INPUT DROP
 ip6tables -P FORWARD DROP
