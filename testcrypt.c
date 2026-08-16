15bfcd23067183b051b511a5f4fcef2291768b99
riuzenn <riuzenn@outlook.com>
2026-08-17 00:32:59 +0800

    测试libcrypt.so.0库的crypt()函数支持哪些算法

--- testcrypt.c
+++ testcrypt.c
@@ -4,10 +4,13 @@
 
 int main()
 {
-    printf("DES: %s\n", crypt("123456", "ab"));
-    printf("MD5: %s\n", crypt("123456", "$1$testsalt$"));
-    printf("Blowfish: %s\n", crypt("123456", "$2a$05$usesomesillystring$"));
-    printf("SHA256: %s\n", crypt("123456", "$5$testsalt$"));
-    printf("SHA512: %s\n", crypt("123456", "$6$testsalt$"));
+    printf("%-22s %s\n", "DES:", crypt("123456", "ab"));
+    printf("%-22s %s\n", "MD5:", crypt("123456", "$1$testsalt$"));
+    printf("%-22s %s\n", "Blowfish:", crypt("123456", "$2a$05$usesomesillystring$"));
+    printf("%-22s %s\n", "SHA256:", crypt("123456", "$5$testsalt$"));
+    printf("%-22s %s\n", "SHA512:", crypt("123456", "$6$testsalt$"));
+    printf("%-22s %s\n", "Blowfish(fallback):", crypt("123456", "$2"));
+    printf("%-22s %s\n", "SHA256(fallback):", crypt("123456", "$5"));
+    printf("%-22s %s\n", "SHA512(fallback):", crypt("123456", "$6"));
     return 0;
 }
